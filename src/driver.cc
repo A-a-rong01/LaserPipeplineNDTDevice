84208302ff8a7c573988e698a22465954aaac6f2
Gabe Lapingcao <mgultra2018@gmail.com>
2026-02-15 01:28:40 -0800

    Implemented options using cxxoptions header library

--- src/driver.cc
+++ src/driver.cc
@@ -1,5 +1,6 @@
-#include "include/image_processor.h"
 #include "include/cpp_image_processor.h"
+#include "include/cxxopts.hpp"
+#include "include/image_processor.h"
 #include "include/py_image_processor.h"
 
 #include <algorithm>
@@ -9,11 +10,38 @@
 
 int main(int argc, char *argv[])
 {
+    cxxopts::Options options("NDT Image Analyzer", "Non-destructive testing "
+        "image processing software developed for detecting microcracks and "
+        "micro pits on the Raspberry Pi 5");
+
+    options.add_options()
+        ("p,python", "Run with the embedded Python interpreter")
+        ("h,help", "Print usage")
+    ;
+
+    auto result = options.parse(argc, argv);
+
+    // return early on help
+    if (result.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+        return EXIT_SUCCESS;
+    }
+    
     // turn the arguments into a vector of strings
-    std::vector<std::string> args(argv + 1, argv + argc);
+    // std::vector<std::string> args(argv + 1, argv + argc);
 
     ImageProcessor *processor = nullptr;
-    if (std::find(args.begin(), args.end(), "--python") != args.end())
+    // if (std::find(args.begin(), args.end(), "--python") != args.end())
+    // {
+    //     processor = new PyImageProcessor;
+    // }
+    // else
+    // {
+    //     processor = new CppImageProcessor;
+    // }
+
+    if(result["python"].as<bool>())
     {
         processor = new PyImageProcessor;
     }
@@ -23,5 +51,5 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "processor->getName() = " << processor->getName() << std::endl;
-    return 0;
+    return EXIT_SUCCESS;
 }
